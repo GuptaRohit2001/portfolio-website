afbcdd3c511e13e7ccfc4f7610facd951a1a434e
GuptaRohit2001 <imrohitgupta2001@gmail.com>
2026-03-01 13:47:33 +0530

    Footer section updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className='py-8 bg-dark-300'>
       <div className='container mx-auto px-6 text-center'>
-        <p className='text-gray-400'>© 2026 Created By TheRohitGuptaCodes. All rights reserved.</p>
+        <p className='text-gray-400'>© {currentYear} Created By Rohit Gupta. All rights reserved.</p>
       </div>
     </div>
   )
